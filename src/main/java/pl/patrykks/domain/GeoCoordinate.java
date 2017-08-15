1c09964381e97f411bd12737b2cba18fdc9114d3
patrykks <pskalsk@student.agh.edu.pl>
2017-08-15 21:04:36 +0200

    Change of default deserialization behaviour - ignore unknown properties

--- src/main/java/pl/patrykks/domain/GeoCoordinate.java
+++ src/main/java/pl/patrykks/domain/GeoCoordinate.java
@@ -1,7 +1,9 @@
 package pl.patrykks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GeoCoordinate {
 
     @JsonProperty(value = "lat")
